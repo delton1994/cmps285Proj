5abb8183978c7c780ac0cb13d769d7c04d3d493d
Nikki T <nikita.thapaliya@selu.edu>
2019-03-18 12:22:48 -0500

    Creating user result table

---
+++ ProgrammingCamp.API/StarterProject.Api/Data/Entites/UserResult.cs
@@ -0,0 +1,16 @@
+﻿using StarterProject.Api.Features.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterProject.Api.Data.Entites
+{
+    public class UserResult
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+        public int Result { get; set; }
+    }
+}
