02d0e4ae314ca3cbba8a367775f57f802661bf80
Burnell Hall <burnellhall17@gmail.com>
2019-03-29 01:38:11 -0500

    Adding language links to Results tab &  page layout

---
+++ ProgrammingCamp.Web/src/features/layout/Results.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import Page from './Page';
+import {ROUTES} from '../../constants';
+import {NavLink} from 'react-router-dom';
+import './Results.css'
+
+class Results extends Component {
+  render(){
+    return (
+      <Page header="Your Results">
+        <div className='main-div'>
+          <div className='left-box'>
+           <nav className='left-nav'>
+            <ul className='navList'>
+            <NavLink
+              className='language-link'
+              exact
+              activeClassName='selected'
+              to={ROUTES.HTML}
+            >
+              HTML
+            </NavLink>
+
+            <NavLink
+              className='language-link'
+              exact
+              activeClassName='selected'
+              to={ROUTES.Java}
+            >
+              Java
+            </NavLink>
+
+            <NavLink
+              className='language-link'
+              exact
+              activeClassName='selected'
+              to={ROUTES.JavaScript}
+            >
+              JavaScript
+            </NavLink>
+
+            <NavLink
+              className='language-link'
+              exact
+              activeClassName='selected'
+              to={ROUTES.CSharp}
+            >
+              C#
+            </NavLink>
+            </ul>
+          </nav>
+          </div>
+          <div className='center-card'>
+          This is the middle card...
+          </div>
+          <div className='right-box'>
+            Hello
+          </div>
+        </div>
+      </Page>
+    );
+  }
+}
+
+export default Results;
